03193effece9188d91fbaed74be7bd09b106145e
anandanraju <anandandurairaju@gmail.com>
2024-05-01 12:18:56 +0530

    Folder Added

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
 import setuptools
 
-with open("README.md","r",encoding="utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
+
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Kidney-Disease-Classification-DL-MLFlow-DVC-Project"
+REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
 AUTHOR_USER_NAME = "anandanraju"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "anandandurairaju@gmail.com"
 
 
-setuptools.setup
-(
-    name=SRC_REPO,
+setuptools.setup(
+    __name__=SRC_REPO,
     version=__version__,
-    author=AUTHOR_USER_NAME
+    author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A small python package for CNN App",
+    description="A small python package for CNN app",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"":"src"},
+    license="MIT",
+    package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
